f74faea557b079846ff95db199ea1168d752923c
Zach Hill <zach@anchore.com>
2019-09-27 12:00:05 -0700

    Try using exec actions helper

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const core = require('@actions/core');
 const execSync = require('child_process').execSync;
-//const exec = require('@actions/exec');
+const exec = require('@actions/exec');
+
 
 // most @actions toolkit packages have async methods
 async function run() {
     try {
-        var output = execSync('ls', {encoding: 'utf-8'});  // the default is 'buffer'
+        let output = execSync('ls', {encoding: 'utf-8'});  // the default is 'buffer'
         console.log('ls Output was:\n', output);
 
         output = execSync('pwd', {encoding: 'utf-8'});  // the default is 'buffer'
@@ -16,11 +17,25 @@ async function run() {
 
         core.debug((new Date()).toTimeString());
         core.addPath('.');
-        var img = core.getInput('image_reference');
-        var cmd = 'inline_scan-v0.5.0 ' + img;
-        //await exec.exec(cmd);
-        output = execSync(cmd, {encoding: 'utf-8'});  // the default is 'buffer'
-        console.log('Output was:\n', output);
+        let img = core.getInput('image_reference');
+        let cmd = 'inline_scan-v0.5.0 ' + img;
+        let myOutput = '';
+        let myError = '';
+
+        const options = {};
+        options.listeners = {
+            stdout: (data: Buffer) => {
+                myOutput += data.toString();
+            },
+            stderr: (data: Buffer) => {
+                myError += data.toString();
+            }
+        };
+        options.cwd = './lib';
+        await exec.exec(cmd, options);
+        //output = execSync(cmd, {encoding: 'utf-8'});  // the default is 'buffer'
+        console.log('Output was:\n', myOutput);
+        console.log('Error was:\n', myError);
 
         core.debug((new Date()).toTimeString());
 
