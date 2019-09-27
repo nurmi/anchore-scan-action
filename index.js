cd9e1d3aa5c574d35d87b9695eb59550d7a8429d
Zach Hill <zach@anchore.com>
2019-09-27 11:41:54 -0700

    Slightly broken first cut at calling anchore inline scan via js

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const core = require('@actions/core');
-const wait = require('./wait');
-
+const execSync = require('child_process').execSync;
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    try {
+        const ms = core.getInput('milliseconds');
+        console.log(`Waiting ${ms} milliseconds ...`);
 
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+        core.debug((new Date()).toTimeString());
+        var img = core.getInput('image_reference');
+        var cmd = './inline_scan-v0.5.0 ' + img;
+        const output = execSync(cmd, {encoding: 'utf-8'});  // the default is 'buffer'
+        console.log('Output was:\n', output);
 
-    core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
-    core.setFailed(error.message);
-  }
+        core.debug((new Date()).toTimeString());
+
+        core.setOutput('time', new Date().toTimeString());
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
-run()
+run();
